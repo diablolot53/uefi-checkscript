236ac28ffb18b7fc25fe16853e344bf2ba6cb0de
Ken Maurer <diablolot53@gmail.com>
2017-09-21 13:43:09 -0400

    *Added a fix for Windows 7 *Created a notification window for non-UEFI machines

--- uefi-checkscript/uefi-checkscript/uefi-checkscript.ps1
+++ uefi-checkscript/uefi-checkscript/uefi-checkscript.ps1
@@ -9,34 +9,85 @@ Param (
 	[switch]$SecureBoot = $false    #Checks the status of SecureBoot in addition to UEFI | Default: off
 )
 
+#-------------------------------------------------------------
 
 #Functions
 Function Get-BiosType {
-	#Function is from the GetFirmwareType.ps1 script - https://gallery.technet.microsoft.com/scriptcenter/Determine-UEFI-or-Legacy-7dc79488
-<#
-.Synopsis
-   Determines underlying firmware (BIOS) type and returns an integer indicating UEFI, Legacy BIOS or Unknown.
-   Supported on Windows 8/Server 2012 or later
-.DESCRIPTION
-   This function uses a complied Win32 API call to determine the underlying system firmware type.
-.EXAMPLE
-   If (Get-BiosType -eq 1) { # System is running UEFI firmware... }
-.EXAMPLE
-    Switch (Get-BiosType) {
-        1       {"Legacy BIOS"}
-        2       {"UEFI"}
-        Default {"Unknown"}
+	#Function is derived from the code provided in the GetFirmwareType.ps1 script by Chris J Warwick - https://gallery.technet.microsoft.com/scriptcenter/Determine-UEFI-or-Legacy-7dc79488
+	[OutputType([UInt32])]
+	Param()
+
+	If ((Get-CimInstance win32_OperatingSystem).Caption -like "*Windows 7*"){
+		#If Win7
+		<#
+		.Synopsis
+		   Determines underlying firmware (BIOS) type and returns True for UEFI or False for legacy BIOS.
+		.DESCRIPTION
+		   This function uses a complied Win32 API call to determine the underlying system firmware type.
+		.EXAMPLE
+		   If (IsUEFI) { # System is running UEFI firmware... }
+		.OUTPUTS
+		   [Bool] True = UEFI Firmware; False = Legacy BIOS
+		.FUNCTIONALITY
+		   Determines underlying system firmware type
+		#>
+
+	Add-Type -Language CSharp -TypeDefinition @'
+
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class CheckUEFI
+    {
+        [DllImport("kernel32.dll", SetLastError=true)]
+        static extern UInt32 
+        GetFirmwareEnvironmentVariableA(string lpName, string lpGuid, IntPtr pBuffer, UInt32 nSize);
+
+        const int ERROR_INVALID_FUNCTION = 1; 
+
+        public static uint IsUEFI()
+        {
+            // Try to call the GetFirmwareEnvironmentVariable API.  This is invalid on legacy BIOS.
+
+            GetFirmwareEnvironmentVariableA("","{00000000-0000-0000-0000-000000000000}",IntPtr.Zero,0);
+
+            if (Marshal.GetLastWin32Error() == ERROR_INVALID_FUNCTION)
+
+                return 1;     // API not supported; this is a legacy BIOS
+
+            else
+
+                return 2;      // API error (expected) but call is supported.  This is UEFI.
+        }
     }
-.OUTPUTS
-   Integer indicating firmware type (1 = Legacy BIOS, 2 = UEFI, Other = Unknown)
-.FUNCTIONALITY
-   Determines underlying system firmware type
-#>
+'@
 
-[OutputType([UInt32])]
-Param()
 
-Add-Type -Language CSharp -TypeDefinition @'
+		[CheckUEFI]::IsUEFI()
+	}
+	Else{
+		#If Win8 or later
+		<#
+		.Synopsis
+		   Determines underlying firmware (BIOS) type and returns an integer indicating UEFI, Legacy BIOS or Unknown.
+		   Supported on Windows 8/Server 2012 or later
+		.DESCRIPTION
+		   This function uses a complied Win32 API call to determine the underlying system firmware type.
+		.EXAMPLE
+		   If (Get-BiosType -eq 1) { # System is running UEFI firmware... }
+		.EXAMPLE
+			Switch (Get-BiosType) {
+				1       {"Legacy BIOS"}
+				2       {"UEFI"}
+				Default {"Unknown"}
+			}
+		.OUTPUTS
+		   Integer indicating firmware type (1 = Legacy BIOS, 2 = UEFI, Other = Unknown)
+		.FUNCTIONALITY
+		   Determines underlying system firmware type
+		#>
+
+	Add-Type -Language CSharp -TypeDefinition @'
 
     using System;
     using System.Runtime.InteropServices;
@@ -58,7 +109,8 @@ Add-Type -Language CSharp -TypeDefinition @'
 '@
 
 
-    [FirmwareType]::GetFirmwareType()
+		[FirmwareType]::GetFirmwareType()
+	}
 }
 
 Function DisplayWindow {
@@ -136,6 +188,7 @@ Function DisplayWindow {
 	$Form.ShowDialog() | out-null
 }
 
+#-------------------------------------------------------------
 
 #Query the system for the BIOS status
 $UEFIResults = Switch (Get-BiosType) {
@@ -147,7 +200,19 @@ $UEFIResults = Switch (Get-BiosType) {
 #Check for SecureBoot if UEFI is enabled and the SecureBoot parameter is defined
 If (($UEFIResults -eq "UEFI") -and ($SecureBoot -eq $true)){
 	Try{$SecureBootResult = Confirm-SecureBootUEFI -ErrorAction SilentlyContinue}
+		<#
+		Confirm-SecureBootUEFI
+		If the computer supports Secure Boot and Secure Boot is enabled, then this cmdlet returns True. 
+		If the computer supports Secure Boot and Secure Boot is disabled, then this cmdlet returns False. 
+		#>
 	Catch{$SecureBootResult = "No"}
+		#If the computer does not support Secure Boot or is a BIOS (non-UEFI) computer, then this cmdlet returns an error displaying the following: Cmdlet not supported on this platform.
+}
+ElseIf (($UEFIResults -eq "Legacy BIOS") -and ($SecureBoot -eq $true)){
+	$SecureBootResult = "Not Supported"
+}
+Else{
+	$SecureBootResult = "Not Checked"
 }
 
 #Display Results if the Debug parameter is set
@@ -201,4 +266,42 @@ If ($Debug -eq $True){
 
 	#Display the window
 	DisplayWindow -inputXML $window -OkButtonName "OkButton" -labelText $labels
+}
+Else{
+	#Return return results of the check
+	#If UEFI is not enabled, display a notification and exit the script with an error
+	If ($UEFIResults -ne "UEFI"){
+		#Display a notification window
+		$window = @"
+<Window x:Class="WpfDebugWindow.UEFINotification"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns:local="clr-namespace:WpfDebugWindow"
+        mc:Ignorable="d"
+        Title="UEFI Error" Height="187.5" Width="349.794" WindowStartupLocation="CenterScreen" ResizeMode="NoResize">
+    <Grid Margin="10,10,8,8">
+        <Label Content="UEFI is currently disabled&#xD;&#xA;" Margin="19,10,0,0" VerticalAlignment="Top" Height="27" Width="250" HorizontalContentAlignment="Center" HorizontalAlignment="Left"/>
+        <Label Content="Please reboot the system and enable UEFI from the &#xD;&#xA;firmware interface" Margin="10,37,4,0" VerticalAlignment="Top" Height="38" FontSize="11"/>
+        <Button x:Name="OkButton" Content="Ok" Margin="114,93,0,10" Width="94" HorizontalContentAlignment="Center" VerticalContentAlignment="Center" HorizontalAlignment="Left"/>
+
+    </Grid>
+</Window>
+"@
+
+		DisplayWindow -inputXML $window -OkButtonName "OkButton"
+
+		#Exit with an error code that will cause the task sequence to fail
+		Exit 1
+	}
+
+	#Display a notification if UEFI is enabled but Secureboot is turned off
+	If (($SecureBoot -eq $true) -and ($SecureBootResult -eq $false)){
+		#Display a notification window
+		
+	}
+
+	#If UEFI is enabled and SecureBoot check passed (if checked) then exit the script w/o any error codes
+	Exit
 }
